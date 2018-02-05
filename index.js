a160380d01683e1f85c027d233c5c5a70e87def5
Lauri Junkkari <latenssi@gmail.com>
2018-02-05 17:17:37 +0200

    Add some functionality

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const {
+  createServer,
+  makeTemplatesController,
+  makeTemplatesModel,
+  makeTemplatesRouter
+} = require("./lib");
+
+const mockDBConnection = {
+  model: () => ({
+    find: () => []
+  })
+};
+
+const server = createServer({});
+
+const templatesModel = makeTemplatesModel({ dbConnection: mockDBConnection });
+const templatesCtrl = makeTemplatesController({ model: templatesModel });
+const templatesRouter = makeTemplatesRouter({ controller: templatesCtrl });
+
+templatesRouter.applyRoutes(server, "/templates");
+
+server.listen(8080, function() {
+  process.stdout.write("Server listening...\n");
+});
