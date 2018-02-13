464f7f7843405276f086f153cf1fcbec9471d7a8
Lauri Junkkari <latenssi@gmail.com>
2018-02-13 13:34:47 +0200

    Reorder things

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ const config = require("config");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Router = require("restify-router").Router;
+const Web3 = require("web3");
 
+const { ProxyWalletContract } = require("./lib/contracts");
 const {
-  createServer,
   makeTemplatesController,
-  makeTemplatesModel,
-  makeTemplatesRouter,
-  makeProxyWalletsController,
-  makeProxyWalletsRouter
-} = require("./lib");
+  makeProxyWalletsController
+} = require("./lib/controllers");
+const { makeTemplatesModel } = require("./lib/models");
+const { makeTemplatesRouter, makeProxyWalletsRouter } = require("./lib/routes");
+const { createServer } = require("./lib/server");
 
 // const mockDBConnection = {
 //   model: () => ({
@@ -34,12 +35,21 @@ const templatesRouter = makeTemplatesRouter({
   controller: templatesCtrl,
   Router
 });
-const proxyWalletsCtrl = makeProxyWalletsController();
+templatesRouter.applyRoutes(server, "/templates");
+
+// ProxyWallet setup
+const web3 = new Web3();
+web3.setProvider(
+  new web3.providers.HttpProvider(config.get("web3.providers.HttpProvider"))
+);
+const proxyWalletsCtrl = makeProxyWalletsController({
+  web3,
+  ProxyWalletContract
+});
 const proxyWalletsRouter = makeProxyWalletsRouter({
   controller: proxyWalletsCtrl,
   Router
 });
-templatesRouter.applyRoutes(server, "/templates");
 proxyWalletsRouter.applyRoutes(server, "/proxywallet");
 
 // Start server
