5f40f76af98fe32640c5b19188ee623d7e6720ee
Lauri Junkkari <latenssi@gmail.com>
2018-02-15 08:55:59 +0200

    Improve testing

--- wallaby.js
+++ wallaby.js
@@ -1,4 +1,6 @@
 module.exports = function() {
+  // process.env.NODE_ENV = "test";
+
   return {
     files: [
       {
@@ -19,15 +21,21 @@ module.exports = function() {
         load: true,
         ignore: false
       },
+      {
+        pattern: "lib/utils/test_setup.js",
+        instrument: false,
+        load: true,
+        ignore: false
+      },
       { pattern: "lib/**/*.js", instrument: true, load: true, ignore: false }
     ],
     tests: ["lib/**/*.test.js"],
     env: {
       type: "node",
-      runner: "node",
-      params: {
-        env: "NODE_ENV=test"
-      }
+      runner: "node"
+    },
+    bootstrap: function() {
+      require("./lib/utils/test_setup");
     }
   };
 };
